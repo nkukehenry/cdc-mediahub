6602cca66485a2acc7beff86d0e16021261ffcf2
Henry NKuke <henry.nkuke@interswitchng.com>
2025-11-08 15:00:57 +0300

    deployment alternative

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  basePath: '/mediahub',
-  assetPrefix: '/mediahub',
-  trailingSlash: false, // let Next.js add the trailing slash
-  reactStrictMode: true,
+  // basePath: '/mediahub',
+  // assetPrefix: '/mediahub',
+  // trailingSlash: false, // let Next.js add the trailing slash
+  // reactStrictMode: true,
 }
 
 module.exports = nextConfig
