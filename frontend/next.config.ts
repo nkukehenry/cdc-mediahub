3af281675eaaf6ae8f0830a1cd188cc242fb9914
Henry NKuke <henry.nkuke@interswitchng.com>
2025-11-08 12:17:20 +0300

    use basepath in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  // optional: if you have a trailing slash preference
-  //basePath: '/mediahub',
-  // assetPrefix: '/mediahub',
-  // trailingSlash: false,
-  // reactStrictMode: true,
+  basePath: '/mediahub',
+  trailingSlash: true, // let Next.js add the trailing slash
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
